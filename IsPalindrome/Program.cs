eed3d6059dabb3c2a4b09fdf7a6e4f505c5692a9
Toppedyk <Tayloroppedyk@yahoo.com>
2024-05-06 09:28:55 -0600

    first commit

---
+++ IsPalindrome/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IsPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a word: ");
+            string str = Console.ReadLine();
+            char[] reversed = str.ToCharArray();
+            Array.Reverse(reversed);
+            string reversedString = new string(reversed).ToUpper();
+            string upper = str.ToUpper();
+            if(reversedString == upper){
+            Console.WriteLine(str + " is a palindrome");
+            }else{
+            Console.WriteLine(str + " is not a palindrome");
+            }
+        }
+    }
+}
