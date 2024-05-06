02e8cf79ecba2db2ea6d449966e765a3777fb6b4
Toppedyk <Tayloroppedyk@yahoo.com>
2024-05-06 09:39:52 -0600

    reverse word orer

---
+++ ReverseWordOrder/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ReverseWordOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter words and I'll reverse their order: ");
+            string s = Console.ReadLine();
+            string[] words = s.Split(" ");
+            Console.Write("The reversed order of the words are: ");
+            for(int i = words.Length-1; i>=0;i--){
+                Console.Write(words[i] + " ");
+            }
+        }
+    }
+}
