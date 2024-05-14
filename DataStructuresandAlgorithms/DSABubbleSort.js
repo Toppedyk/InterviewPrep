7147ea33aa146fba4d3bff07c8fd957e4c1228cc
Toppedyk <Tayloroppedyk@yahoo.com>
2024-05-14 08:00:08 -0600

    improving bubble sort

--- DataStructuresandAlgorithms/DSABubbleSort.js
+++ DataStructuresandAlgorithms/DSABubbleSort.js
@@ -1,10 +1,15 @@
-let my_array = [64, 34, 25, 12, 22, 11, 90, 5];
+let my_array = [7, 3, 9, 12, 11];
 for (let i = 0; i < my_array.length - 1; i++) {
+  var swapped = false;
   for (let j = 0; j < my_array.length - i - 1; j++) {
     if (my_array[j] > my_array[j + 1]) {
       var temp = my_array[j];
       my_array[j] = my_array[j + 1];
       my_array[j + 1] = temp;
+      swapped = true;
+    }
+    if (!swapped) {
+      break;
     }
   }
 }
