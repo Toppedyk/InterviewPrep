02b0df18d0c338c0dff98a15d8786256370d494d
Toppedyk <Tayloroppedyk@yahoo.com>
2024-06-27 17:48:47 -0600

    join method

--- ArrayMethods/methods.js
+++ ArrayMethods/methods.js
@@ -7,4 +7,6 @@ console.log(size);
 let sentence = fruits.toString();
 console.log(sentence);
 
-//
+// Join array elements to string and define seperator
+let joined = fruits.join("*");
+console.log(joined);
