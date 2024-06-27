ae4ba2cdfa9167da6221f66e2bb7f6e3825ba4fb
Toppedyk <Tayloroppedyk@yahoo.com>
2024-06-27 17:46:49 -0600

    toString

--- ArrayMethods/methods.js
+++ ArrayMethods/methods.js
@@ -3,4 +3,8 @@ const fruits = ["Banana", "Orange", "Apple", "Mango"];
 let size = fruits.length;
 console.log(size);
 
+// Convert array to string
+let sentence = fruits.toString();
+console.log(sentence);
+
 //
